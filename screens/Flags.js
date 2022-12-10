f5ae25495f010afdca847171887322ddf49f52bb
Terrence <tpaiement@my.bcit.ca>
2022-12-09 22:11:16 -0800

    Terms and Flags

--- screens/Flags.js
+++ screens/Flags.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Button, Image, ScrollView } from "react-native"
 import { useNavigation } from "@react-navigation/native";
 import flagsData from "../data/flags.json";
 import { SvgXml } from 'react-native-svg';
-
+import { AntDesign } from '@expo/vector-icons';
 
 const FlagsScreen = () => {
     const navigation = useNavigation();
@@ -11,28 +11,52 @@ const FlagsScreen = () => {
     
     return (
         <ScrollView>
-        <Text>FlagsScreen</Text>
-        <Button
-            title="Go to Home"
-            onPress={() => navigation.navigate("Home")}
-        />
+            <View style={styles.header}> 
+            <AntDesign name="caretleft" size={24} color="black" onPress={() => navigation.navigate("Home")} />
+                <Text style={styles.headerText}>LGBTQA+ Flags</Text>
+            </View>
         {flags.map((flag) => (
-            <View key={flag.name}>
-                <Text>{flag.name}</Text>
-                <SvgXml xml= {flag.svg} style={{width: 200, height: 200}} />
+            <View key={flag.name}style={styles.container}>
+                <Text style={styles.title}>{flag.name}</Text>
+                <SvgXml xml= {flag.svg} style={{width: 200, height: 200, marginTop: 0}} />
             </View>
             ))}
         </ScrollView>
     );
-    }
+}
 
-const styles = StyleSheet.create({  
+ const styles = StyleSheet.create({  
     container: {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+        marginBottom: 0,
+    },
+    header: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        paddingVertical: 30,
+        backgroundColor: "#dfeeff",
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginVertical: 8,
+    },
+    backarrow: {
+        backgroundColor: "#fff",
+        color: "#000",
+        fontSize: 24,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: -20,
     },
 });
 
 export default FlagsScreen;
-    
